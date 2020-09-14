c47377f24746209d6eec14d42f553ad070f93f73
Adithya <adithya.santhosh33@gmail.com>
2020-09-14 06:38:39 +0530

    added to middleware validateIfUserAndModelAreRelated to delete and photo upload route as well

--- middleware/validateIfUserAndModelAreRelated.js
+++ middleware/validateIfUserAndModelAreRelated.js
@@ -3,31 +3,33 @@ let Course = require("../models/Course");
 const ErrorResponse = require("../utils/ErrorResponse");
 let asyncHandler = require("../middleware/asyncHandler");
 
-module.exports = asyncHandler( async (req, res, next) => {
-//    console.log("lplpl",req.originalUrl); 
-   console.log("baseUrl", req.baseUrl);
-//    console.log("pathUrl", req.path);
+module.exports = (model) => asyncHandler( async (req, res, next) => {
+
     let Model = "";
-    if(req.baseUrl == '/api/v1/bootcamps'){
+    if(model == 'Bootcamp'){
         Model = Bootcamp;
     }
 
-    if(req.baseUrl == '/api/v1/courses'){
+    if(model == 'Course'){
         Model = Course;
     }
 
     if(!Model){
-        console.log("The middleware validateIfUserAndModelAreRelated Seems to be user in a new route".bgRed);
+        console.log("The middleware validateIfUserAndModelAreRelated does not seen to have a model".bgRed);
         process.exit(0)
     }
 
-    let bootcamp = await Model.findById(req.params.id);
+    let modalData = await Model.findById(req.params.id);
+
+    if (!modalData) {
+        return next(new ErrorResponse(`No ${Model.modelName} found with the Id ${req.params.id}`, 404));
+    }
 
     //@ only the owner of the bootcamp or an admin user can update this bootcamp
-    if(bootcamp.user.toString() == req.user.id || req.user.role == "admin"){
+    if(modalData.user.toString() == req.user.id || req.user.role == "admin"){
         next()
     }else{
-        next(new ErrorResponse(`You are not authorized to update this bootcamp as you are not the owner of this bootcamp`))
+        next(new ErrorResponse(`You are not authorized to update this bootcamp as you are not the owner of this bootcamp`,401))
     }
 
 })
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,17 +11,17 @@ const courseRouter = require("../routes/courses");
 const advancedResults = require("../middleware/advancedResults");
 const Bootcamp = require("../models/Bootcamp");
 
-router.use("/:bootcampId/courses", courseRouter)
+router.use("/:bootcampId/courses", courseRouter);
 
 var { getAllBootcamps, getBootcamp, addBootcamp, updateBootcamp, deleteBootcamp, uploadBootcampPhoto } = require("../controllers/bootcamps");
 const asyncHandler = require("../middleware/asyncHandler");
 
-router.get("/",advancedResults(Bootcamp, 'courses'), getAllBootcamps);
-router.get("/:id",advancedResults(Bootcamp, 'courses') ,getBootcamp);
+router.get("/", advancedResults(Bootcamp, 'courses'), getAllBootcamps);
+router.get("/:id", advancedResults(Bootcamp, 'courses'), getBootcamp);
 router.post("/", protect, authorizeRoles("publisher", "admin"), addBootcamp);
-router.put("/:id", protect, authorizeRoles("publisher", "admin"), validateIfUserAndModelAreRelated, updateBootcamp);
-router.delete("/:id",protect, authorizeRoles("publisher", "admin"),deleteBootcamp);
-router.put("/:id/photo",protect, authorizeRoles("publisher", "admin"), uploadBootcampPhoto);
+router.put("/:id", protect, authorizeRoles("publisher", "admin"), validateIfUserAndModelAreRelated("Bootcamp"), updateBootcamp);
+router.delete("/:id", protect, authorizeRoles("publisher", "admin"), validateIfUserAndModelAreRelated("Bootcamp"), deleteBootcamp);
+router.put("/:id/photo", protect, authorizeRoles("publisher", "admin"), validateIfUserAndModelAreRelated("Bootcamp"), uploadBootcampPhoto);
 
 
 module.exports = router;
